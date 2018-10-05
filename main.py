1926a1e742bf6c1e28b0467164cd735417db7e5d
Logan <sirauronsquall@gmail.com>
2018-10-05 11:41:25 -0500

    Added new library for email validation and finished with validation

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from flask import Flask, request, redirect, render_template
+import re
 import cgi
+from email_validator import validate_email, EmailNotValidError
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+
+def isValidEmail(email):
+    try:
+        v = validate_email(email) # validate and get info
+        email = v["email"] # replace with normalized form
+        return True
+    except EmailNotValidError as e:
+        # email is not valid, exception message is human-readable
+        print(str(e))
+        return False
+    
 
 #Check user sign up
 @app.route("/register", methods=['POST'])
@@ -18,23 +31,33 @@ def register_validate():
     # if the user typed nothing at all, redirect and tell them the error
     if (not user) or (user.strip() == ""):
         error = "Please specify name."
-        return redirect("/?error=" + error)
+        return redirect("/?error=" + errorn)
+
+    if (len(user)<4) or (len(user)>20):
+        error = "Valid name is more than 3 characters and less than 20."
+        return redirect("/?error=" + errorn)
+
+    
+    if isValidEmail(email) == False :
+        error = "Please specify valid email."
+        return redirect("/?error=" + errore)
+                    
 
     if (not email) or (email.strip() == ""):
         error = "Please specify email."
-        return redirect("/?error=" + error)
+        return redirect("/?error=" + errore)
 
     if (not password) or (password.strip() == ""):
         error = "Please use a password."
-        return redirect("/?error=" + error)
+        return redirect("/?error=" + errorp)
 
     if (not passwordConfirm) or (passwordConfirm.strip() == ""):
         error = "Please use a password."
-        return redirect("/?error=" + error)
+        return redirect("/?error=" + errorp)
 
     if (passwordConfirm !=password):
         error = "Passwords don't match."
-        return redirect("/?error=" + error)
+        return redirect("/?error=" + errorp)
 
     # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
     user_escaped = cgi.escape(user, quote=True)
@@ -46,7 +69,7 @@ def register_validate():
     # Create a template called add-confirmation.html inside your /templates directory
     # Use that template to render the confirmation message instead of this temporary message below
     encoded_error = request.args.get("error")
-    return render_template('homepage.html', error=encoded_error and cgi.escape(encoded_error, quote=True))
+    return render_template('homepage.html', username=user_escaped, error=encoded_error and cgi.escape(encoded_error, quote=True))
 
 @app.route("/")
 def index():
